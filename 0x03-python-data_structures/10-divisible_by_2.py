0e47ef5161ec4a87abecdea5e1e2d776d973eb17
Med-Slh <mohamed.salhaouii@gmail.com>
2023-06-12 04:28:12 -0700

    10. Only by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    newlist = []
+    for el in my_list:
+        if el % 2 == 0:
+            newlist.append(True)
+        else:
+            newlist.append(False)
+    return newlist
