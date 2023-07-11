bb68f1b277b9e300ebaec268e3333948b0c0c391
Med-Slh <mohamed.salhaouii@gmail.com>
2023-07-11 14:19:35 -0700

    1. Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""a function that writes a string to a text file (UTF8)
-and returns the number of characters written"""
+"""Definition of a func that returns
+num of charcters written"""
 
 
 def write_file(filename="", text=""):
+    """fnc that returns num of charcters written"""
     with open(filename, mode="w", encoding="utf-8") as f:
         return f.write(text)
