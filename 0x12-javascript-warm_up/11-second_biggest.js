0d342854c099ed30f0439f0e5d549a8bce671505
Med-Slh <mohamed.salhaouii@gmail.com>
2023-09-13 14:22:47 -0700

    test

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const arg = process.argv;
+if (arg.length === 2 || arg.length === 3) {
+  console.log('0');
+} else {
+  let arr = [];
+  for (let i = 2; i < process.argv.length; i++) {
+    arr.push(parseInt(process.argv[i]));
+  }
+  arr = arr.sort((a, b) => a - b);
+  console.log(arr[arr.length - 2]);
+}
