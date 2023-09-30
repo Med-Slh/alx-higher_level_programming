63e296b624ee76e8b8c1a247041aa0c2a3aba1b6
Med-Slh <mohamed.salhaouii@gmail.com>
2023-09-29 17:19:11 -0700

    first task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
-"""
-This module fetches https://alx-intranet.hbtn.io/status
-
-Example:
-    $ ./0-hbtn_status.py
-
-"""
+'''a script that fetches https://alx-intranet.hbtn.io/status'''
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import urllib.request
-    with urllib.request.urlopen(
-            'https://alx-intranet.hbtn.io/status') as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type:", type(body))
-        print("\t- content:", body)
-        print("\t- utf8 content:", body.decode('utf-8'))
+
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        data = html.decode('utf-8')
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(data))
